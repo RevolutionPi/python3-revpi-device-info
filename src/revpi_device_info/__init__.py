ea0f0c552891b7418860843981c1d484c697b905
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2023-10-05 13:24:17 +0200

    refactor: Fix linter warning "unknown import"
    Directly import the variables and and ignore warning about unused imports.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- src/revpi_device_info/__init__.py
+++ src/revpi_device_info/__init__.py
@@ -3,7 +3,7 @@
 # Copyright 2022-2023 KUNBUS GmbH
 
 """Hat EEPROM device info."""
-from .__about__ import *
+from .__about__ import __author__, __copyright__, __license__, __version__  # noqa: F401
 from .device_info import RevPiDeviceInfo, RevPiHatEEPROMException
 
 __all__ = ["RevPiDeviceInfo", "RevPiHatEEPROMException"]
